c82692ce19d47019ac3937d5081a8e720de4788e
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-10-31 14:53:46 +0100

    Add safer to dependencies

--- src/install_packages.R
+++ src/install_packages.R
@@ -7,7 +7,8 @@ required <- c("dplyr", "readr", "stringr", "tidyr",
               "httr", "ows4R", # to get data via WFS
               "sf", # to convert coordinate system
               "knitr", # to extract R code from Rmd in run_*.R files
-              "testthat" # to run tests in test_dwc_occurrence.R
+              "testthat", # to run tests in test_dwc_occurrence.R
+              "safer" # to encrypt and decrypt the `Opmerkingen` from raw data
 )
 # install packages if needed
 if (!all(required %in% installed)) {
