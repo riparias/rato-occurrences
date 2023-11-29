b859f4abac239b46cc33b23ce7687dd2ca65478c
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-11-29 16:41:40 +0100

    Functions to experiment with the REST API using authentication

---
+++ src/auth_api_experiments.R
@@ -0,0 +1,111 @@
+# experimental script to retreive rato data behind authentication, via a map
+# service layer
+
+
+# load libraries ----------------------------------------------------------
+
+
+library(httr2)
+
+
+
+
+# get an access token -----------------------------------------------------
+
+get_token <- function(username = "RATO_INBO", password = askpass::askpass()) {
+  token_request <-
+    request("https://gis.oost-vlaanderen.be/portal/sharing/rest/generateToken") %>%
+    req_body_form(
+      username = username,
+      password = password,
+      client = "referer",
+      referer = "https://gis.oost-vlaanderen.be",
+      expiration = 5,
+      f = "json"
+    )
+
+  token_response <-
+    token_request %>%
+    req_perform() %>%
+    resp_body_json()
+
+  return(token_response$token)
+}
+
+# get a list of all object id's -------------------------------------------
+# "query?where=1%3D1&returnIdsOnly=true&f=pjson&token={TOKEN}"
+request("https://gis.oost-vlaanderen.be/server/rest/services/RATO/RATO_Private_Data/MapServer/0/query?where=1%3D1") %>%
+  req_url_query(
+    returnIdsOnly = "true",
+    f = "pjson",
+    token = get_token()
+  ) %>%
+  req_perform() %>%
+  resp_body_json(check_type = FALSE)
+
+get_all_object_ids <- function(token = get_token()) {
+  object_ids_response <-
+    request("https://gis.oost-vlaanderen.be/server/rest/services/RATO/RATO_Private_Data/MapServer/0/query?where=1%3D1") %>%
+    req_url_query(
+      returnIdsOnly = "true",
+      f = "pjson",
+      token = token
+    ) %>%
+    req_perform() %>%
+    resp_body_json(check_type = FALSE)
+
+  object_ids_response %>%
+    purrr::chuck("objectIds") %>%
+    unlist() %>%
+    return()
+}
+
+# get featrues via objectid -----------------------------------------------
+
+object_ids_to_query <- get_all_object_ids()
+
+fetch_objects <- function(object_ids, token = get_token()) {
+  assertthat::assert_that(assertthat::not_empty(object_ids))
+
+  object_id_query <- glue::glue(
+    "OBJECTID IN ({object_ids_collated})",
+    object_ids_collated = glue::glue_collapse(object_ids,
+      sep = ","
+    )
+  ) %>%
+    curl::curl_escape()
+
+  objects_request <-
+    request(
+      glue::glue("https://gis.oost-vlaanderen.be/server/rest/services/RATO/RATO_Private_Data/MapServer/0/query?where={object_id_query}")
+    ) %>%
+    req_url_query(
+      outFields = "*",
+      f = "json",
+      token = token
+    )
+
+  objects_response <-
+    objects_request %>%
+    req_perform() %>%
+    resp_body_json()
+
+  assertthat::assert_that(
+    isTRUE(purrr::pluck(objects_response, "error", "message", .default = TRUE)),
+    msg = purrr::pluck(objects_response, "error", "message")
+  )
+
+  objects_response %>%
+    purrr::chuck("features") %>%
+    purrr::map(~ purrr::pluck(.x, "attributes")) %>%
+    purrr::map_dfr(~.x) %>%
+    return()
+}
+
+
+# split up the ids in batches of 100 --------------------------------------
+object_ids_to_query <- get_all_object_ids()
+
+raw_data <-
+  split(object_ids_to_query, ceiling(seq_along(object_ids_to_query)/100)) %>%
+  purrr::map_dfr(fetch_objects)
