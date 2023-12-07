cdd719cb5c30f595f27ed64694c0fd174a4cabb5
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-12-07 13:31:11 +0100

    Create function to read an encrypted csv file

---
+++ R/read_encrypted_csv.R
@@ -0,0 +1,28 @@
+#' Read an encrypted csv
+#'
+#' The decryption key is read from the system environment by default, but can
+#' be provided as a length one character vector.
+#'
+#' @param ... passed on to [readr::read_csv()]
+#'
+#' @inheritParams readr::read_csv
+#' @inheritParams safer::decrypt_file
+#'
+#' @return A [tibble()]. As per [readr::read_csv()]
+#' @export
+#'
+read_encrypted_csv <- function(file, key = Sys.getenv("encryption_key"), ...) {
+  withr::with_tempfile(
+    "decrypted",
+    {
+      safer::decrypt_file(
+        file,
+        key = key,
+        outfile = decrypted
+      )
+
+      decrypted_tibble <- readr::read_csv(decrypted, ...)
+    }
+  )
+  return(decrypted_tibble)
+}
