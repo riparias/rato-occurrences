cb247e7c0b51dad681a7e0a9540a110bbc333b8c
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-12-08 10:32:28 +0100

    test decryption

--- tests/testthat/test-write_encrypted_csv.R
+++ tests/testthat/test-write_encrypted_csv.R
@@ -47,3 +47,32 @@ test_that("write_encrypted_csv() can write an encrypted csv", {
   )
 })
 
+test_that("write_encrypted_csv() can write a file that can be decrypted", {
+  withr::with_tempfile(
+    c("encrypted_csv", "decrypted_csv"),
+    {
+      object_to_encrypt <- dplyr::tibble(a = seq(1,50), b = seq(50,1))
+      testing_key <- "super public key for testing and not for actual use"
+
+      write_encrypted_csv(
+        object_to_encrypt,
+        outfile = encrypted_csv,
+        key = testing_key
+      )
+
+      safer::decrypt_file(
+        encrypted_csv,
+        key = testing_key,
+        outfile = decrypted_csv
+      )
+      # decryption works
+      expect_true(file.exists(decrypted_csv))
+      # object is about the same after decryption, readr parsing will change
+      # colclasses
+      expect_equal(
+        readr::read_csv(decrypted_csv, show_col_types = FALSE),
+        object_to_encrypt
+      )
+    }
+  )
+})
