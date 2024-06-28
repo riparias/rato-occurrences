db32784f1e69f01360ec8e8958b89abf386b39cd
PietrH <pieterhuy@gmail.com>
2024-06-28 15:50:08 +0200

    Add `Procambarus` to species test list

--- tests/test_dwc_occurrence.R
+++ tests/test_dwc_occurrence.R
@@ -197,7 +197,8 @@ testthat::test_that("scientificName is never NA and one of the list", {
     "Ludwigia grandiflora",
     "Sciurus",
     "Crassula helmsii",
-    "Zizania latifolia"
+    "Zizania latifolia",
+    "Procambarus"
   )
   testthat::expect_true(all(!is.na(dwc_occurrence$scientificName)))
   testthat::expect_true(all(dwc_occurrence$scientificName %in% species))
