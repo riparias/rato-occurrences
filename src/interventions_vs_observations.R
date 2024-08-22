7be1c87122f2fbf92373014ff2db05cf797cd425
PietrH <pieterhuy@gmail.com>
2024-08-22 09:27:40 +0200

    attempt intervention mapping for rats

--- src/interventions_vs_observations.R
+++ src/interventions_vs_observations.R
@@ -37,3 +37,63 @@ plant_records <-
 # QUESTION: does this mean that the remaining records are observations?
 
 
+# Animals -----------------------------------------------------------------
+
+animal_records <- filter(raw_occurrences, Domein == "Dier")
+
+
+
+## The first record of a case ----------------------------------------------
+
+  # is always an observation, it can be an absence, and it can be an
+  # intervention as well!
+
+  # The first record of a case is the one where Laatst_Bewerkt_Datum is the lowest
+  # for a given Dossier_ID
+
+
+# animal_records %>%
+#   group_by(Dossier_ID) %>%
+#   summarise(
+#     dossier_create_date = min(Laatst_Bewerkt_Datum)
+#   ) %>%
+#   ungroup() %>%
+#   filter(first_intervention) %>%
+#   select(Dossier_ID, first_action, first_intervention)
+
+## Change within a case ----------------------------------------------------
+
+
+# Did the action change relative to the previous action of the same case
+
+animal_records %>%
+  group_by(Dossier_ID) %>%
+  summarise(first_action = first(Actie),
+            .groups = "keep") %>%
+  mutate(
+    previous_action = lag(Actie),
+    action_changed = ifelse(Actie != previous_action & !first_action, TRUE, FALSE)
+  ) %>%
+  ungroup() %>%
+  filter(action_changed) %>%
+  select(Dossier_ID, Actie, previous_action, action_changed)
+
+## is the action in the list? ----------------------------------------------
+
+animal_intervention_actions <-
+    c(
+      "vang",
+      "schud",
+      "bestreden",
+      "behandeld"
+    )
+
+animal_records %>%
+  filter(stringr::str_detect(Actie,
+                             paste(animal_intervention_actions,
+                                   collapse = "|")
+                             )
+         ) %>%
+
+
+# QUESTION: what to do with nevenvangsten?
