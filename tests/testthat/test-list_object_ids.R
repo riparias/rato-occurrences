8cf94f5fb2292fc6a2e1449c5e0968b7ec4a4132
PietrH <pieterhuy@gmail.com>
2024-10-29 16:56:13 +0100

    add test for west vlaanderen ecosysteem database

--- tests/testthat/test-list_object_ids.R
+++ tests/testthat/test-list_object_ids.R
@@ -3,3 +3,16 @@ test_that("list_object_ids() works for default RATO oost-vlaanderen API", {
   # there are more than 10.000 records in the RATO Oost-Vlaanderen database
   expect_gte(length(list_object_ids()), 10000)
 })
+
+test_that("list_object_ids() can retreive ids for West-Vlaanderen", {
+  expect_type(
+    list_object_ids(
+      token = get_token(username = "Ecosystem2_INBO",
+                        password = askpass::askpass(),
+                        domain = "https://gwadmin.west-vlaanderen.be/gwportal"),
+      domain = "https://gwadmin.west-vlaanderen.be/gwserver/rest/services",
+      table_path = c("Ecosystem2","AGS_ES2_Dossiers_Publiek","MapServer")
+    ),
+    "integer"
+  )
+})
