675df5ebc43ed7d8a25a2b1be5469ac13e7a7d0b
PietrH <pieterhuy@gmail.com>
2024-12-13 16:00:51 +0100

    cache response and set col classes

--- R/get_reference.R
+++ R/get_reference.R
@@ -20,8 +20,43 @@ get_reference <- function() {
 
   httr2::request(github_csv_url) %>%
     httr2::req_retry() %>%
+    httr2::req_cache(path = tempdir(), max_age = 600) %>%
     httr2::req_perform() %>%
-    httr2::resp_body_string() %>%
+    httr2::resp_body_raw() %>%
+    rawToChar() %>%
     readr::read_csv(show_col_types = FALSE,
-                    progress = FALSE)
+                    progress = FALSE,
+                    col_types = 
+                      readr::cols(
+                        occurrenceID = readr::col_character(),
+                        occurrenceStatus = readr::col_character(),
+                        eventID = readr::col_double(),
+                        scientificName = readr::col_character(),
+                        eventDate = readr::col_datetime(format = ""),
+                        organismQuantity = readr::col_double(),
+                        organismQuantityType = readr::col_character(),
+                        samplingProtocol = readr::col_character(),
+                        samplingEffort = readr::col_character(),
+                        municipality = readr::col_character(),
+                        verbatimLatitude = readr::col_double(),
+                        verbatimLongitude = readr::col_double(),
+                        verbatimCoordinateSystem = readr::col_character(),
+                        verbatimSRS = readr::col_character(),
+                        decimalLatitude = readr::col_double(),
+                        decimalLongitude = readr::col_double(),
+                        coordinateUncertaintyInMeters = readr::col_double(),
+                        countryCode = readr::col_character(),
+                        geodeticDatum = readr::col_character(),
+                        type = readr::col_character(),
+                        language = readr::col_character(),
+                        license = readr::col_character(),
+                        rightsHolder = readr::col_character(),
+                        datasetID = readr::col_character(),
+                        institutionCode = readr::col_character(),
+                        datasetName = readr::col_character(),
+                        basisOfRecord = readr::col_character(),
+                        recordedBy = readr::col_character(),
+                        kingdom = readr::col_character(),
+                        taxonRank = readr::col_character()
+                      ))
 }
