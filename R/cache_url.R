524ea12094250519cba0086a045fde76020df0e0
PietrH <pieterhuy@gmail.com>
2024-12-18 10:54:31 +0100

    Add docs for `cache_url()`

--- R/cache_url.R
+++ R/cache_url.R
@@ -1,3 +1,12 @@
+#' Cache URL content
+#'
+#' This function fetches the content of a given URL and caches it for future use.
+#' If the provided path is not an URL, it returns the path so it can be used directly by reading functions.
+#'
+#' @param url The URL to fetch the content from.
+#' @inheritParams httr2::req_cache
+#' @return The content of the URL, either fetched as a string. If the provided path is not a url, it is returned as is.
+#' @export
 cache_url <- function(url, debug = FALSE) {
   # If the provided path is not an URL, return it so it can be used directly by reading functions
   if (!R.utils::isUrl(url)) {
